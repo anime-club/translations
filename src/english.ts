32db5eec20c73ab84c99891fa2bd8bcc2c23989c
Olzhas Alexandrov <olzhas@alexandrov.co>
2022-07-22 23:30:10 -0500

    FIXED: `logout` noun => `logOut` verb

--- src/english.ts
+++ src/english.ts
@@ -136,7 +136,7 @@ export const text = {
   livesIn: `Lives in`,
   location: `Location`,
   logInOrSignUp: `Log in or sign up`,
-  logout: `Logout`,
+  logOut: `Log out`,
   maximumNChars: (num: number) => `Maximum ${num} chars`,
   maxSize: `Max size`,
   menu: `Menu`,
