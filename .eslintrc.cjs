1c636b500d6e4487ff91140056aea15494c0c6bc
Olzhas Alexandrov <olzhas@alexandrov.co>
2023-10-30 05:50:19 -0500

    CHANGED: use `ML` translation on non-translated keys

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,6 @@ module.exports = {
   root: true,
   parserOptions: { project: true, tsconfigRootDir: __dirname },
   extends: ["./node_modules/@10stars/config/.eslintrc"],
-  overrides: [
-    {
-      files: ["src/*"],
-      rules: {
-        "sort-keys-fix/sort-keys-fix": [1, "asc", { caseSensitive: false }],
-      },
-    },
-  ],
   rules: {
     "@typescript-eslint/restrict-template-expressions": 0,
   },
