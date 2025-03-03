6f3d6c95d448eb53d35fee99fd6657758497fff2
Olzhas Alexandrov <9992724+o-alexandrov@users.noreply.github.com>
2025-03-02 19:24:08 -0600

    CHANGED: upgrade eslint

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  parserOptions: { project: true, tsconfigRootDir: __dirname },
-  extends: ["./node_modules/@10stars/config/.eslintrc"],
-  rules: {
-    "@typescript-eslint/restrict-template-expressions": 0,
-  },
-}
